c1125dbe79bc3d6c047d63b0d2d86041fda30735
missdatauz <mahliyoshermetova12@gmail.com>
2025-12-27 12:55:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+textInput = document.getElementById("text-input");
+checkButton = document.getElementById("check-btn");
+textResult = document.getElementById("result");
+
+checkButton.addEventListener("click", () => {
+    textResult.innerText = "Please input a value"
+})
