9952d28e55650314d35bef0dcda972cfb990d15d
missdatauz <mahliyoshermetova12@gmail.com>
2026-01-07 11:09:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-textInput = document.getElementById("text-input");
-checkButton = document.getElementById("check-btn");
-textResult = document.getElementById("result");
+let textInput = document.getElementById("text-input");
+let checkBtn = document.getElementById("check-btn");
+let result = document.getElementById("result");
 
-checkButton.addEventListener("click", () => {
-    textResult.innerText = "Please input a value"
-})
+checkBtn.addEventListener("click", palindrome);
+
+function palindrome() {
+  if (textInput.value == "") {
+    alert("Please input a value");
+    result.innerHTML = "";
+    return;
+  }
+
+  const processedStr = textInput.value.replace(/[\W_]/g, "").toLowerCase();
+  let isPalindrome = true;
+
+  for (let i = 0; i < Math.floor(processedStr.length / 2); i++) {
+    if (processedStr[i] !== processedStr[processedStr.length - 1 - i]) {
+      isPalindrome =false;
+      break;
+    }
+  }
+
+  if (isPalindrome) {
+    result.innerHTML = textInput.value + " is a palindrome";
+  } else {
+    result.innerHTML = textInput.value + " is not a palindrome"
+  }
+}
